4011cbc540556e79b76f5bc719851cdf41faf9a9
k-chin-abist <k_chin@abist.co.jp>
2025-12-15 03:54:14 +0900

    Initial Commit

---
+++ testMeasure/testMeasureApp.swift
@@ -0,0 +1,17 @@
+//
+//  testMeasureApp.swift
+//  testMeasure
+//
+//  Created by jacky72503 on 2025/12/15.
+//
+
+import SwiftUI
+
+@main
+struct testMeasureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testMeasureTests/testMeasureTests.swift
@@ -0,0 +1,17 @@
+//
+//  testMeasureTests.swift
+//  testMeasureTests
+//
+//  Created by jacky72503 on 2025/12/15.
+//
+
+import Testing
+@testable import testMeasure
+
+struct testMeasureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
